708740bae262d2f1f027ffbbb75403dfff257292
seohyunk09 <2022112400@dgu.ac.kr>
2026-09-24 03:52:43 +0900

    chore: 백엔드 프로젝트 생성 및 빌드/테스트 환경 구성

---
+++ backend/src/main/java/com/richreach/RichReachApplication.java
@@ -0,0 +1,13 @@
+package com.richreach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichReachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RichReachApplication.class, args);
+    }
+
+}
